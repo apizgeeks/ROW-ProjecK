e72a881fec926601afa854dfe0abeedbfad1731f
apizgeeks <86207806+apizgeeks@users.noreply.github.com>
2026-05-19 00:26:36 +0800

    Add files via upload

---
+++ ScriptEngine/CompilerMFC/resource.h
@@ -0,0 +1,22 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Developer Studio generated include file.
+// Used by CompilerMFC.rc
+//
+#define IDD_COMPILERMFC_DIALOG          102
+#define IDR_MAINFRAME                   128
+#define IDC_EDIT_INPUT                  1000
+#define IDC_BUTTON_BROWSE1              1001
+#define IDC_EDIT_OUTPUT                 1002
+#define IDC_BUTTON_BROWSE2              1003
+#define IDC_EDIT_MSG                    1004
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        129
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1005
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
